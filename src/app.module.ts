37805cd60109e70e1f2b401769a67b5d495429dc
dimashikalov <dimashikalov86@mail.ru>
2025-02-18 15:24:19 +0300

    added yml + mongoose + test models

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,18 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/mongotest'),
     ConfigModule.forRoot(),
     AuthModule,
     TopPageModule,
     ProductModule,
     ReviewModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
